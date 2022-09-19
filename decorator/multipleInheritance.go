53fcd04abbd8816f54006b0e7d40d113a819f1f8
Stefan <stefanamoah001@gmail.com>
2022-09-19 23:57:18 +0200

    refactor: multiple inheritance

--- decorator/multipleInheritance.go
+++ decorator/multipleInheritance.go
@@ -30,71 +30,71 @@ type Dragon struct {
 */
 
 type Aged interface {
-  Age() int
-  SetAge(age int)
+	Age() int
+	SetAge(age int)
 }
 
 type Bird struct {
-  age int
+	age int
 }
 
-func (b *Bird) Age() int { return b.age }
+func (b *Bird) Age() int       { return b.age }
 func (b *Bird) SetAge(age int) { b.age = age }
 
 func (b *Bird) Fly() {
-  if b.age >= 10 {
-    fmt.Println("Flying!")
-  }
+	if b.age >= 10 {
+		fmt.Println("Flying!")
+	}
 }
 
 type Lizard struct {
-  age int
+	age int
 }
 
-func (l *Lizard) Age() int { return l.age }
+func (l *Lizard) Age() int       { return l.age }
 func (l *Lizard) SetAge(age int) { l.age = age }
 
 func (l *Lizard) Crawl() {
-  if l.age < 10 {
-    fmt.Println("Crawling!")
-  }
+	if l.age < 10 {
+		fmt.Println("Crawling!")
+	}
 }
 
 type Dragon struct {
-  bird Bird
-  lizard Lizard
+	bird   Bird
+	lizard Lizard
 }
 
 func (d *Dragon) Age() int {
-  return d.bird.age
+	return d.bird.age
 }
 
 func (d *Dragon) SetAge(age int) {
-  d.bird.SetAge(age)
-  d.lizard.SetAge(age)
+	d.bird.SetAge(age)
+	d.lizard.SetAge(age)
 }
 
 func (d *Dragon) Fly() {
-  d.bird.Fly()
+	d.bird.Fly()
 }
 
 func (d *Dragon) Crawl() {
-  d.lizard.Crawl()
+	d.lizard.Crawl()
 }
 
 func NewDragon() *Dragon {
-  return &Dragon{Bird{}, Lizard{}}
+	return &Dragon{Bird{}, Lizard{}}
 }
 
 func main() {
-  //d := Dragon{}
-  //d.Bird.Age = 10
-  //fmt.Println(d.Lizard.Age)
-  //d.Fly()
-  //d.Crawl()
+	//d := Dragon{}
+	//d.Bird.Age = 10
+	//fmt.Println(d.Lizard.Age)
+	//d.Fly()
+	//d.Crawl()
 
-  d := NewDragon()
-  d.SetAge(10)
-  d.Fly()
-  d.Crawl()
+	d := NewDragon()
+	d.SetAge(10)
+	d.Fly()
+	d.Crawl()
 }
