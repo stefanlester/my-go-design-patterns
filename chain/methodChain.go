895b294156bd3e8e503bf3d1253bd96d3e1b60c9
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-11-05 20:54:35 +0100

    cnt: chain of responsibilty; method chain

--- chain/methodChain.go
+++ chain/methodChain.go
@@ -91,3 +91,22 @@ func NewNoBonusesModifier(
 func (n *NoBonusesModifier) Handle() {
   // nothing here!
 }
+
+func main() {
+	goblin := NewCreature("Goblin", 1, 1)
+	fmt.Println(goblin.String())
+  
+	root := NewCreatureModifier(goblin)
+  
+	//root.Add(NewNoBonusesModifier(goblin))
+  
+	root.Add(NewDoubleAttackModifier(goblin))
+	root.Add(NewIncreasedDefenseModifier(goblin))
+	root.Add(NewDoubleAttackModifier(goblin))
+  
+  
+  
+	// eventually process the entire chain
+	root.Handle()
+	fmt.Println(goblin.String())
+  }
